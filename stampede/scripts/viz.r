25e82fd780dfa2cd6855e036192a86df1e845f86
Ken Youens-Clark <kyclark@gmail.com>
2016-09-14 11:09:33 -0500

    handles aliases

--- stampede/scripts/viz.r
+++ stampede/scripts/viz.r
@@ -5,26 +5,24 @@ library("vegan")
 library("igraph")
 library("networkD3")
 library("R.utils")
+library("hash")
 
 # --------------------------------------------------
-fix_dist = function (file, alias_file) {
+fix_dist = function (file, alias_file="") {
   lines = readLines(file)
   n = length(lines)
   delim = "\t"
   fixed = file.path(dirname(file), paste0(basename(file), '.fixed'))
 
-  aliases = ""
-  if (length(alias_file) > 0 & file.exists(alias_file)) {
-    printf("Using alias file '%s'\n", alias_file)
-    aliases = read.table(alias_file, header=T, as.is=T)
-    for (i in 1:length(labels)) {
-      label = labels[i]
-      alias = aliases[aliases$name == label, "alias"]
-      if (length(alias) > 0) {
-        printf("Alias '%s' -> '%s'\n", label, alias)
-        labels[i] = alias
-      }
-    }
+  aliases = NULL
+  if (nchar(alias_file) > 0 && file.exists(alias_file)) {
+    aliases_df = read.table(alias_file, header=T, as.is=T)
+    aliases = hash(aliases_df$name, aliases_df$alias)
+  }
+
+  get_basename = function (x) { 
+    b = basename(x)
+    if (is.hash(aliases) && has.key(b, aliases)) aliases[[b]] else b 
   }
 
   sink(fixed)
@@ -35,12 +33,12 @@ fix_dist = function (file, alias_file) {
     # also, the fields are full paths we need to reduce to basenames
     if (i == 1) {
       header = flds[2:n]
-      basenames = unlist(lapply(header, basename))
+      basenames = unlist(lapply(header, get_basename))
       cat(paste(c("", basenames), collapse=delim), sep="\n")
     }
     # the first column has full path, so convert to basename
     else {
-      cat(paste(c(basename(flds[1]), flds[2:n]), collapse=delim), sep="\n")
+      cat(paste(c(get_basename(flds[1]), flds[2:n]), collapse=delim), sep="\n")
     }
   }
   sink()
@@ -54,12 +52,12 @@ main = function () {
 
   option_list = list(
     make_option(c("-f", "--file"),
-                default=NULL, 
+                default="", 
                 type="character", 
                 help="distance matrix", 
                 metavar="character"),
     make_option(c("-o", "--outdir"),
-                default=NULL, 
+                default="", 
                 type="character", 
                 help="outdir", 
                 metavar="character"),
@@ -84,7 +82,7 @@ main = function () {
   }
 
   dist_file = opt$file
-  out_dir   = if (length(opt$outdir) > 1) opt$outdir else dirname(dist_file)
+  out_dir   = if (nchar(opt$outdir) > 1) opt$outdir else dirname(dist_file)
   dist      = fix_dist(dist_file, opt$alias)
   work_dir  = opt$workdir
 
