d45fdedfda289388d1ce671d2a03dc95a66f93c6
Ken Youens-Clark <kyclark@gmail.com>
2016-10-06 18:03:44 -0500

    one day this might work

--- stampede/template.sh
+++ stampede/template.sh
@@ -2,4 +2,9 @@
 
 set -u
 
-./00-controller.sh -f ${FASTA_DIR} -m ${METADATA_FILE} -o ${OUT_DIR:-"mash-out"} -d ${EUC_DIST_PERCENT:0.1} -s ${SAMPLE_DIST:1000}
+META=""
+if [[ ${#METADATA_FILE} -gt 0 ]]; then
+  META="-m ${METADATA_FILE}"
+fi
+
+./00-controller.sh -f ${FASTA_DIR} -o ${OUT_DIR:-"mash-out"} -d ${EUC_DIST_PERCENT:-0.1} -s ${SAMPLE_DIST:-1000} -x ${NUM_SCANS:-100000} $META
