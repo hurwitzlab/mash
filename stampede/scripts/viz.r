bb4515e3dc2d19bacd25259496e72c872f43bc88
Ken Youens-Clark <kyclark@gmail.com>
2016-12-02 13:03:54 -0700

    no more fix_dist

--- stampede/scripts/viz.r
+++ stampede/scripts/viz.r
@@ -8,44 +8,6 @@ library("R.utils")
 library("hash")
 
 # --------------------------------------------------
-fix_dist = function (file, alias_file="") {
-  lines = readLines(file)
-  n = length(lines)
-  delim = "\t"
-  fixed = file.path(dirname(file), paste0(basename(file), '.fixed'))
-
-  aliases = NULL
-  if (nchar(alias_file) > 0 && file.exists(alias_file)) {
-    aliases_df = read.table(alias_file, header=T, as.is=T)
-    aliases = hash(aliases_df$name, aliases_df$alias)
-  }
-
-  get_basename = function (x) { 
-    b = basename(x)
-    if (is.hash(aliases) && has.key(b, aliases)) aliases[[b]] else b 
-  }
-
-  sink(fixed)
-  for (i in 1:n) {
-    flds = strsplit(lines[i], delim)[[1]]
-    
-    # the header line has a comment char "#" we need to strip
-    # also, the fields are full paths we need to reduce to basenames
-    if (i == 1) {
-      header = flds[2:n]
-      basenames = unlist(lapply(header, get_basename))
-      cat(paste(c("", basenames), collapse=delim), sep="\n")
-    }
-    # the first column has full path, so convert to basename
-    else {
-      cat(paste(c(get_basename(flds[1]), flds[2:n]), collapse=delim), sep="\n")
-    }
-  }
-  sink()
-  return(read.table(fixed))
-}
-
-# --------------------------------------------------
 main = function () { 
   cwd = getwd()
   setwd(cwd)
@@ -83,7 +45,7 @@ main = function () {
 
   dist_file = opt$file
   out_dir   = if (nchar(opt$outdir) > 1) opt$outdir else dirname(dist_file)
-  dist      = fix_dist(dist_file, opt$alias)
+  dist      = read.table(dist_file)
   work_dir  = opt$workdir
 
   if (!file.exists(out_dir)) {
