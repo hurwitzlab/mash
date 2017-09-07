a18b7dcffc8710c11f50e58b48da653757aaf676
Ken Youens-Clark <kyclark@gmail.com>
2017-09-07 11:37:00 -0700

    working

--- scripts/install.r
+++ scripts/install.r
@@ -2,10 +2,11 @@
 
 install.packages("ggplot2")
 install.packages("hash")
-install.packages("igraph")
-install.packages("networkD3")
 install.packages("optparse")
 install.packages("plyr")
 install.packages("R.utils")
 install.packages("vegan")
 install.packages("xtable")
+library(devtools)
+install_github("igraph/rigraph")
+install.packages("networkD3")
