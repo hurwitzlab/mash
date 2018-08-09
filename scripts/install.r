688c37b08b9de369708ac7c9e1c2bbde1815ac1b
Ken Youens-Clark <kyclark@gmail.com>
2018-08-09 16:28:57 -0700

    changed reqs

--- scripts/install.r
+++ scripts/install.r
@@ -1,14 +1,22 @@
 #!/usr/bin/env Rscript
 
-install.packages("ggplot2")
-install.packages("hash")
-install.packages("optparse")
-install.packages("plyr")
-install.packages("R.utils")
-install.packages("vegan")
-install.packages("xtable")
-install.packages("shiny")
-install.packages("devtools")
-library(devtools)
-install_github("igraph/rigraph")
-install.packages("networkD3")
+install.packages("optparse");
+install.packages("ggdendro");
+install.packages("ggplot2");
+install.packages("vegan");
+install.packages("R.utils");
+install.packages("reshape2");
+install.packages("ape");
+
+#install.packages("ggplot2")
+#install.packages("hash")
+#install.packages("optparse")
+#install.packages("plyr")
+#install.packages("R.utils")
+#install.packages("vegan")
+#install.packages("xtable")
+#install.packages("shiny")
+#install.packages("devtools")
+#library(devtools)
+#install_github("igraph/rigraph")
+#install.packages("networkD3")
