5652428ed5b38ce748f5410612089bbe69fe40d4
Ken Youens-Clark <kyclark@gmail.com>
2018-08-10 13:29:54 -0700

    let config.query have multiple, use aliases in sketching, print better errors w/missing filenames

--- run_mash/src/lib.rs
+++ run_mash/src/lib.rs
@@ -8,7 +8,7 @@ use std::collections::HashMap;
 use std::error::Error;
 use std::process::{Command, Stdio};
 use std::{
-    env, fs::{self, DirBuilder, File}, io::{self, Write}, path::{Path, PathBuf},
+    env, fs::{self, DirBuilder, File}, io::Write, path::{Path, PathBuf},
 };
 use walkdir::WalkDir;
 
@@ -24,7 +24,7 @@ pub struct Config {
     euc_dist_percent: Option<String>,
     num_threads: u32,
     out_dir: PathBuf,
-    query: String,
+    query: Vec<String>,
 }
 
 type MyResult<T> = Result<T, Box<Error>>;
@@ -63,7 +63,8 @@ pub fn get_args() -> MyResult<Config> {
                 .long("query")
                 .value_name("FILE_OR_DIR")
                 .help("File or input directory")
-                .required(true),
+                .required(true)
+                .min_values(1),
         )
         .arg(
             Arg::with_name("out_dir")
@@ -112,8 +113,6 @@ pub fn get_args() -> MyResult<Config> {
         )
         .get_matches();
 
-    let query = String::from(matches.value_of("query").unwrap());
-
     let out_dir = match matches.value_of("out_dir") {
         Some(x) => PathBuf::from(x),
         //_ => None,
@@ -161,28 +160,29 @@ pub fn get_args() -> MyResult<Config> {
         euc_dist_percent: euc_dist_percent,
         num_threads: num_threads,
         out_dir: out_dir,
-        query: query,
+        query: matches.values_of_lossy("query").unwrap(),
     };
 
     Ok(config)
 }
 
 // --------------------------------------------------
-fn find_files(path: &str) -> Result<Vec<String>, Box<Error>> {
-    let meta = fs::metadata(path)?;
-    let files = if meta.is_file() {
-        vec![path.to_owned()]
-    } else {
-        let mut files = vec![];
-        for entry in fs::read_dir(path)? {
-            let entry = entry?;
-            let meta = entry.metadata()?;
-            if meta.is_file() {
-                files.push(entry.path().display().to_string());
+fn find_files(paths: &Vec<String>) -> Result<Vec<String>, Box<Error>> {
+    let mut files = vec![];
+    for path in paths {
+        let meta = fs::metadata(path)?;
+        if meta.is_file() {
+            files.push(path.to_owned());
+        } else {
+            for entry in fs::read_dir(path)? {
+                let entry = entry?;
+                let meta = entry.metadata()?;
+                if meta.is_file() {
+                    files.push(entry.path().display().to_string());
+                }
             }
-        }
-        files
-    };
+        };
+    }
 
     if files.len() == 0 {
         return Err(From::from("No input files"));
@@ -198,11 +198,11 @@ fn sketch_files(config: &Config, files: &Vec<String>) -> MyResult<Vec<String>> {
         DirBuilder::new().recursive(true).create(&sketch_dir)?;
     }
 
+    let aliases = get_aliases(&config.alias_file)?;
     let mut jobs = vec![];
 
     for file in files.iter() {
-        let buf = PathBuf::from(file);
-        let basename = buf.file_name().unwrap();
+        let basename = basename(&file, &aliases);
         let out_file = sketch_dir.join(basename);
         let mash_file = format!("{}.msh", out_file.display());
 
@@ -253,7 +253,7 @@ fn run_jobs(jobs: &Vec<String>, msg: &str, num_concurrent: u32) -> MyResult<()> 
             .arg("--halt")
             .arg("soon,fail=1")
             .stdin(Stdio::piped())
-            .stdout(Stdio::piped())
+            .stdout(Stdio::null())
             .spawn()?;
 
         {
@@ -263,8 +263,12 @@ fn run_jobs(jobs: &Vec<String>, msg: &str, num_concurrent: u32) -> MyResult<()> 
                 .expect("Failed to write to stdin");
         }
 
-        let output = process.wait_with_output().expect("Failed to read stdout");
-        println!("{}", String::from_utf8_lossy(&output.stdout));
+        let result = process.wait()?;
+        if !result.success() {
+            return Err(From::from("Failed to run jobs in parallel"));
+        }
+        //let output = process.wait_with_output().expect("Failed to read stdout");
+        //println!("{}", String::from_utf8_lossy(&output.stdout));
     }
 
     Ok(())
@@ -298,30 +302,44 @@ fn pairwise_compare(config: &Config, sketches: &Vec<String>) -> MyResult<String>
         .arg("-l")
         .arg(all_mash_no_ext)
         .arg(&sketch_list)
-        .output()?;
+        .output();
 
-    if !paste.status.success() {
-        return Err(From::from("Error Mash paste"));
-    }
+    if let Err(e) = paste {
+        return Err(From::from(format!("Failed to run \"mash paste\": {}", e)));
+    };
 
-    let dist = Command::new("mash")
+    let dist_cmd = Command::new("mash")
         .arg("dist")
         .arg("-t")
         .arg(&all_mash)
         .arg(&all_mash)
-        .output()?;
+        .output();
 
-    let aliases = match &config.alias_file {
-        Some(f) => get_aliases(f.to_string()).ok(),
-        None => None,
+    let dist_out = match dist_cmd {
+        Err(e) => {
+            let msg = format!("Failed to run \"mash dist\": {}", e);
+            return Err(From::from(msg));
+        }
+        Ok(res) => String::from_utf8_lossy(&res.stdout).to_string(),
     };
 
-    let dist_out = fix_mash_distance(&String::from_utf8_lossy(&dist.stdout), aliases);
-
-    if dist_out.len() > 0 {
+    let mash_dist = fix_mash_distance(&dist_out);
+    if mash_dist.len() == 0 {
+        println!("Failed to get usable output from \"mash dist\"");
+    } else {
         let dist_file = fig_dir.join("distance.txt");
-        let dist_fh = File::create(&dist_file)?;
-        write!(&dist_fh, "{}", dist_out)?;
+        let dist_fh = match File::create(&dist_file) {
+            Ok(file) => file,
+            Err(e) => {
+                let msg = format!(
+                    "Failed to write \"{}\": {}",
+                    dist_file.to_string_lossy(),
+                    e.to_string()
+                );
+                return Err(From::from(msg));
+            }
+        };
+        write!(&dist_fh, "{}", mash_dist)?;
 
         let make_figures = "make_figures.r";
         let make_figures_path = match &config.bin_dir {
@@ -329,53 +347,56 @@ fn pairwise_compare(config: &Config, sketches: &Vec<String>) -> MyResult<String>
             _ => PathBuf::from(make_figures),
         };
 
+        println!("Making figures");
         let mk_figs = Command::new(&make_figures_path)
             .arg("-o")
             .arg(&fig_dir)
             .arg("-m")
             .arg(&dist_file)
-            .output()?;
+            .output();
 
-        if !mk_figs.status.success() {
-            println!(
-                "Error running {:?}: {:?}",
-                &make_figures_path,
-                String::from_utf8_lossy(&mk_figs.stderr)
+        if let Err(e) = mk_figs {
+            let msg = format!(
+                "Failed to run \"{}\": {}",
+                make_figures_path.to_string_lossy(),
+                e.to_string()
             );
-        }
+            return Err(From::from(msg));
+        };
     }
 
-    fs::remove_file(&sketch_list)?;
-    fs::remove_file(&all_mash)?;
+    fs::remove_file(&sketch_list).unwrap();
+    fs::remove_file(&all_mash).unwrap();
 
     Ok(fig_dir.to_string_lossy().to_string())
 }
 
 // --------------------------------------------------
-fn fix_mash_distance(s: &str, aliases: Option<Record>) -> String {
+fn fix_mash_distance(s: &str) -> String {
     let mut res = vec![];
     for (i, line) in s.split("\n").enumerate() {
         res.push(if i == 0 {
-            fix_mash_header(&line, &aliases)
+            fix_mash_header(&line)
         } else {
-            fix_mash_line(&line, &aliases)
+            fix_mash_line(&line)
         });
     }
+
     res.join("\n")
 }
 
 // --------------------------------------------------
-fn fix_mash_header(line: &str, aliases: &Option<Record>) -> String {
+fn fix_mash_header(line: &str) -> String {
     let mut flds: Vec<&str> = line.split("\t").collect();
     flds[0] = "";
-    let hdrs: Vec<&str> = flds.iter().map(|f| basename(f, aliases)).collect();
+    let hdrs: Vec<&str> = flds.iter().map(|f| basename(f, &None)).collect();
     hdrs.join("\t")
 }
 
 // --------------------------------------------------
-fn fix_mash_line(line: &str, aliases: &Option<Record>) -> String {
+fn fix_mash_line(line: &str) -> String {
     let mut flds: Vec<&str> = line.split("\t").collect();
-    flds[0] = basename(flds[0], aliases);
+    flds[0] = basename(flds[0], &None);
     flds.join("\t")
 }
 
@@ -398,33 +419,50 @@ fn basename<'a>(filename: &'a str, aliases: &'a Option<Record>) -> &'a str {
 }
 
 // --------------------------------------------------
-fn get_aliases(alias_file: String) -> Result<Record, io::Error> {
-    let alias_fh = File::open(&alias_file)?;
-    let mut aliases = HashMap::new();
-    let delimiter = match Path::new(&alias_file).extension() {
-        Some(ext) => match ext.to_str() {
-            Some("csv") => b',',
-            _ => b'\t',
-        },
-        _ => b'\t',
-    };
-    let mut rdr = csv::ReaderBuilder::new()
-        .delimiter(delimiter)
-        .from_reader(alias_fh);
+fn get_aliases(alias_file: &Option<String>) -> Result<Option<Record>, Box<Error>> {
+    match alias_file {
+        None => Ok(None),
+        Some(file) => {
+            let alias_fh = match File::open(file) {
+                Ok(file) => file,
+                Err(e) => {
+                    let msg = format!("Failed to open \"{}\": {}", file, e.to_string());
+                    return Err(From::from(msg));
+                }
+            };
 
-    for result in rdr.deserialize() {
-        let record: Record = result?;
-        let name = record.get("sample_name");
-        let alias = record.get("alias");
+            let mut aliases = HashMap::new();
+            let delimiter = match Path::new(&file).extension() {
+                Some(ext) => match ext.to_str() {
+                    Some("csv") => b',',
+                    _ => b'\t',
+                },
+                _ => b'\t',
+            };
 
-        match (name, alias) {
-            (Some(name), Some(alias)) => {
-                aliases.insert(name.to_string(), alias.to_string());
-                ()
+            let mut rdr = csv::ReaderBuilder::new()
+                .delimiter(delimiter)
+                .from_reader(alias_fh);
+
+            for result in rdr.deserialize() {
+                let record: Record = result?;
+                let name = record.get("sample_name");
+                let alias = record.get("alias");
+
+                match (name, alias) {
+                    (Some(name), Some(alias)) => {
+                        aliases.insert(name.to_string(), alias.to_string());
+                        ()
+                    }
+                    _ => println!("Missing sample_name or alias"),
+                }
             }
-            _ => println!("Missing sample_name or alias"),
+
+            if aliases.len() > 0 {
+                Ok(Some(aliases))
+            } else {
+                Ok(None)
+            }
         }
     }
-
-    Ok(aliases)
 }
